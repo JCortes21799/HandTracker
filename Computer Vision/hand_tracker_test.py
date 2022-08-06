93a8eff2d111341b082c157875a746bde61e968a
JCortes21799 <jcortes@andrew.cmu.edu>
2022-08-05 21:02:03 -0400

    Setup the libraries needed

---
+++ Computer Vision/hand_tracker_test.py
@@ -0,0 +1,15 @@
+import mediapipe as mp #Mediapipe for palm models
+import numpy as np     #Mathetical math operations
+import cv2             #OpenCV for computer vision
+import time
+
+
+
+#Testing grounds for hand tracking
+#Log: 8/5/22: Beginning of project
+
+#Firt thing we will do is simple Hand tracking using libraries
+
+#Second thing will be arm tracking (Skelenton tracking)
+
+#Combine with two camera sources or even just one
