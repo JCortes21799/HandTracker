42b13021dc05d923eba700efef0b1e98e1bff815
JCortes21799 <jcortes@andrew.cmu.edu>
2022-08-05 21:27:48 -0400

    Implements the handedness in the video feed

--- Computer Vision/hand_tracker_test.py
+++ Computer Vision/hand_tracker_test.py
@@ -2,14 +2,37 @@ import mediapipe as mp #Mediapipe for palm models
 import numpy as np     #Mathetical math operations
 import cv2             #OpenCV for computer vision
 import time
-
-
-
+import math
 #Testing grounds for hand tracking
 #Log: 8/5/22: Beginning of project
 
 #Firt thing we will do is simple Hand tracking using libraries
+IM_HEIGHT = 480
+IM_WIDTH = 480
 
-#Second thing will be arm tracking (Skelenton tracking)
+#Resize camera window
+def resize_and_show(image):
+    height, width = image.shape[:2] #Get the image parameters
+    if (height < width):
+        img = cv2.resize(image, IM_WIDTH, math.floor(height/(width/IM_WIDTH)))
+    else:
+        img = cv2.resize(image, math.floor(width/(height/IM_HEIGHT)), IM_HEIGHT)
+    cv2.imshow(img)
 
-#Combine with two camera sources or even just one
+    return
+
+#Here we will be using the mediapipe processing pipeline to draw the digits
+mp_hands = mp.solutions.hands #This is there hand model
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+cam = cv2.VideoCapture(1) #Using the first accesible
+while True:
+    #Running modified mp processing example
+    with mp_hands.Hands(static_image_mode=True, max_num_hands=2, min_detection_confidence=0.5) as hands:
+        success, image = cv2.VideoCapture(1)
+        #Flipping + changing color of image
+        results = hands.process(cv2.flip(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), 1))
+        
+        print("Handness:")
+        print(results.multi_handedness)
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # HandTracker
+Initial commit setting up the repo. This is a testing grounds for a robotic arm project, being moved with different control mechanims.
+
+Project: Ideas
+1. Games with Mom (Easy family friendly game website)
+2. Robotic arm with differnt control methods (CV, Handheld Controller, Tracking an animal)
+    i. Video Ideas with Ahngae for this
+3. Gantry drawing bot (Again, but this time its controlled by a 2d animal (Ahngae))
+4. Roomba Water getting robot
